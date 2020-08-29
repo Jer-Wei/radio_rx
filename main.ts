a38f0c5b334fecc0fa1de017e5724d4f57cffc58
Jer-Wei Rau <Jer-Wei@users.noreply.github.com>
2020-08-30 00:33:39 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,6 @@
 radio.onReceivedValue(function (name, value) {
-    if (name == "temperature") {
-        led.toggle(4, 4)
-        serial.writeValue("temperature", value)
-    }
+    led.toggle(4, 4)
+    serial.writeValue(name, value)
 })
 basic.showString("Radio RX")
 radio.setGroup(200)
-serial.redirectToUSB()

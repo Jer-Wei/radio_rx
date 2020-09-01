ab45f77ff7db74c41584f57124c9ba3b46e99e32
Jer-Wei Rau <Jer-Wei@users.noreply.github.com>
2020-09-01 15:47:53 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,13 @@
 radio.onReceivedValue(function (name, value) {
     led.toggle(4, 4)
     serial.writeValue(name, value)
+    if (name == "Tem") {
+        if (value >= 29) {
+            led.toggle(2, 2)
+        }
+    }
 })
-basic.showString("Radio RX")
+let coordinate = [0, 0]
+let D2MX = [0, coordinate, 0]
+basic.showString("RX")
 radio.setGroup(200)
